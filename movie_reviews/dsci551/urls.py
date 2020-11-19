5aa8672b30ef53dd4ff7d180b625e3dbff9cc06c
Sara Wang <sarawang0829@gmail.com>
2020-11-19 10:44:04 -0800

    Update urls.py
    added path for searchReviews.

--- movie_reviews/dsci551/urls.py
+++ movie_reviews/dsci551/urls.py
@@ -4,4 +4,5 @@ from dsci551 import views
 urlpatterns = [
     path('', views.dsci551_index, name='dsci551_index'),
     path("<int:pk>/", views.dsci551_detail, name="dsci551_detail"),
+    path('searchReviews/', views.search, name = 'dsci551_search'),
 ]
