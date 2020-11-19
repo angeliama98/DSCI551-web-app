a97394cd76f2ada60d08eb61d5b898757687e726
Sara Wang <sarawang0829@gmail.com>
2020-11-19 13:50:50 -0800

    Update views.py
    added connection to firebase and first movie reviews test

--- movie_reviews/dsci551/views.py
+++ movie_reviews/dsci551/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from dsci551.models import Dsci551
+import pyrebase
+
+config = {
+    'apiKey': "AIzaSyD-cFEFVd5wfcXc4AD6hCRiuf1id39yJpg",
+    'authDomain': "project-movie-reviews.firebaseapp.com",
+    'databaseURL': "https://project-movie-reviews.firebaseio.com/",
+    'storageBucket': "project-movie-reviews.appspot.com"
+  }
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+first_reviews = db.child("movie_reviews").child("11").get().val()
 
 # Create your views here.
 def dsci551_index(request):
@@ -20,4 +32,7 @@ def dsci551(request):
     return render(request, 'dsci551.html', {})
 
 def search(request):
-    return render(request, 'dsci551_search.html', {})
+    context = {
+        "first_reviews": first_reviews
+    }
+    return render(request, 'dsci551_search.html', context)
